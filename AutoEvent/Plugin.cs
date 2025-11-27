147830b4b97309a0d27323b769e106cb2a82ca17
MedveMarci <martonmende@gmail.com>
2025-11-27 14:44:52 +0100

    Chnaged version number

--- AutoEvent/Plugin.cs
+++ AutoEvent/Plugin.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
 using AutoEvent.API;
 using AutoEvent.Loader;
 using HarmonyLib;
@@ -18,7 +13,6 @@ namespace AutoEvent;
 
 public class AutoEvent : Plugin<Config>
 {
-    private const bool PreRelease = true;
     public static AutoEvent Singleton;
     private static Harmony _harmonyPatch;
     public static EventManager EventManager;
@@ -32,7 +26,7 @@ public class AutoEvent : Plugin<Config>
     public override string Description =>
         "A plugin that allows you to play mini-games in SCP:SL. It includes a variety of games such as Spleef, Lava, Hide and Seek, Knives, and more. Each game has its own unique mechanics and rules, providing a fun and engaging experience for players.";
 
-    public override Version Version => new(9, 15, 0, 3);
+    public override Version Version => new(9, 15, 0);
     public override Version RequiredApiVersion => new(LabApiProperties.CompiledVersion);
 
     public static string BaseConfigPath { get; private set; }
@@ -111,117 +105,5 @@ public class AutoEvent : Plugin<Config>
         Singleton = null;
         CustomHandlersManager.UnregisterEventsHandler(_eventHandler);
         _eventHandler = null;
-    }
-
-    internal static async Task CheckForUpdatesAsync(Version currentVersion)
-    {
-        try
-        {
-            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-
-            using var client = new HttpClient();
-            client.DefaultRequestHeaders.UserAgent.ParseAdd($"{Singleton.Name}/{currentVersion}");
-            client.DefaultRequestHeaders.Accept.ParseAdd("application/vnd.github+json");
-
-            var repo = $"MedveMarci/{Singleton.Name}";
-            var latestStableJson = await client.GetStringAsync($"https://api.github.com/repos/{repo}/releases/latest")
-                .ConfigureAwait(false);
-            var allReleasesJson = await client
-                .GetStringAsync($"https://api.github.com/repos/{repo}/releases?per_page=20").ConfigureAwait(false);
-
-            using var latestStableDoc = JsonDocument.Parse(latestStableJson);
-            using var allReleasesDoc = JsonDocument.Parse(allReleasesJson);
-
-            var latestStableRoot = latestStableDoc.RootElement;
-            string stableTag = null;
-            if (latestStableRoot.TryGetProperty("tag_name", out var tagProp))
-                stableTag = tagProp.GetString();
-            var stableVer = ParseVersion(stableTag);
-
-            JsonElement? latestPre = null;
-            Version preVer = null;
-            string preTag = null;
-
-            if (allReleasesDoc.RootElement.ValueKind == JsonValueKind.Array)
-            {
-                DateTime? bestPublishedAt = null;
-                foreach (var rel in allReleasesDoc.RootElement.EnumerateArray().Where(rel => rel.ValueKind == JsonValueKind.Object))
-                {
-                    var draft = rel.TryGetProperty("draft", out var draftProp) &&
-                                draftProp.ValueKind == JsonValueKind.True;
-                    if (draft) continue;
-
-                    var prerelease = rel.TryGetProperty("prerelease", out var preProp) &&
-                                     preProp.ValueKind == JsonValueKind.True;
-                    if (!prerelease) continue;
-
-                    DateTime? publishedAt = null;
-                    if (rel.TryGetProperty("published_at", out var pubProp))
-                    {
-                        var s = pubProp.GetString();
-                        if (!string.IsNullOrWhiteSpace(s) && DateTime.TryParse(s, out var dt))
-                            publishedAt = dt;
-                    }
-
-                    if (latestPre != null && (!publishedAt.HasValue ||
-                                              (bestPublishedAt.HasValue && publishedAt.Value <= bestPublishedAt.Value)))
-                        continue;
-                    latestPre = rel;
-                    bestPublishedAt = publishedAt;
-                }
-            }
-
-            if (latestPre.HasValue)
-            {
-                if (latestPre.Value.TryGetProperty("tag_name", out var preTagProp))
-                    preTag = preTagProp.GetString();
-                preVer = ParseVersion(preTag);
-            }
-
-            var outdatedStable = stableVer != null && stableVer > currentVersion;
-            var prereleaseNewer = preVer != null && preVer > currentVersion && !outdatedStable;
-
-            if (outdatedStable)
-                LogManager.Info(
-                    $"A new {Singleton.Name} version is available: {stableTag} (current {currentVersion}). Download: https://github.com/MedveMarci/{Singleton.Name}/releases/latest",
-                    ConsoleColor.DarkRed);
-            else if (prereleaseNewer)
-                LogManager.Info(
-                    $"A newer pre-release is available: {preTag} (current {currentVersion}). Download: https://github.com/MedveMarci/{Singleton.Name}/releases/tag/{preTag}",
-                    ConsoleColor.DarkYellow);
-            else
-                LogManager.Info(
-                    $"Thanks for using {Singleton.Name} v{currentVersion}. To get support and latest news, join to my Discord Server: https://discord.gg/KmpA8cfaSA",
-                    ConsoleColor.Blue);
-            if (PreRelease)
-                LogManager.Info(
-                    "This is a pre-release version. There might be bugs, if you find one, please report it on GitHub or Discord.",
-                    ConsoleColor.DarkYellow);
-        }
-        catch (Exception e)
-        {
-            LogManager.Error($"Version check failed.\n{e}");
-        }
-    }
-
-    private static Version ParseVersion(string tag)
-    {
-        try
-        {
-            if (string.IsNullOrWhiteSpace(tag)) return null;
-            var t = tag.Trim();
-            if (t.StartsWith("v", StringComparison.OrdinalIgnoreCase))
-                t = t.Substring(1);
-
-            var cut = t.IndexOfAny(['-', '+']);
-            if (cut >= 0)
-                t = t.Substring(0, cut);
-
-            return Version.TryParse(t, out var v) ? v : null;
-        }
-        catch
-        {
-            return null;
-        }
     }
 }
